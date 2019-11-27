d77f1622ec9fd70047946938033b14d75d7ec06e
David Carl <david.martin.carl@gmail.com>
2019-11-28 00:04:22 +0100

    grep improvement done, now we need threads part

--- RunThreads.sh
+++ RunThreads.sh
@@ -4,9 +4,8 @@ TOWN0='London'
 
 for i in {1..20}
 do
-   for j in {1..100}
+   for j in {1..20}
    do
-      echo "$i, $j"
-      (time find . -type f -print0 | xargs -0 -P $i grep -wrl '$TOWN0' ./zipfiles/target > /dev/null) 2>> ./res/$i
+      (time find . -type f -print0 | xargs -0 -P $i grep -wrl '$TOWN0' > /dev/null) 2>> ./run_time_data/threads/$i
    done
 done
--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@ TOWN1='Berlin'
 TOWN2='Odense'
 for i in {1..100}
 do
-   echo $i
-   (time grep -wrl '$TOWN0' ./zipfiles/target > /dev/null) 2>> ./res/$TOWN0
-   (time grep -wrl '$TOWN1' ./zipfiles/target > /dev/null) 2>> ./res/$TOWN1
-   (time grep -wrl '$TOWN2' ./zipfiles/target > /dev/null) 2>> ./res/$TOWN2
+   (time grep -wrl '$TOWN0' ./zipfiles/target > /dev/null) 2>> ./run_time_data/$TOWN0
+   (time grep -wrl '$TOWN1' ./zipfiles/target > /dev/null) 2>> ./run_time_data/$TOWN1
+   (time grep -wrl '$TOWN2' ./zipfiles/target > /dev/null) 2>> ./run_time_data/$TOWN2
 done
