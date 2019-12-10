4278cf3798ab1cc970be30bbf41edf74722f0f6b
David Carl <david.martin.carl@gmail.com>
2019-12-10 19:16:45 +0100

    more in NEWREADME.md

--- parallelization.sh
+++ parallelization.sh
@@ -2,5 +2,5 @@
 
 for i in {1..$1} #threads
 do
-      (time grep -wrl 'London' > /dev/null) 2>> ./run_time_data &
+      (time grep -wrl 'London' > /dev/null) 2>> ./run_time_data & (time grep -wrl 'Odense' > /dev/null) 2>> ./run_time_data & (time grep -wrl 'Berlin' > /dev/null) 2>> ./run_time_data & (time grep -wrl 'New York' > /dev/null) 2>> ./run_time_data
 done
